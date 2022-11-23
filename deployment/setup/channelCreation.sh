d70d9eda44a30de42b1c270e7c52a54d6c20f61c
LaughingAdversial <96821823+LaughingAdversial@users.noreply.github.com>
2022-11-23 09:35:30 +0100

    Splitting of script functions
    reducing test time for chaincode deployment etc.

---
+++ deployment/setup/channelCreation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e '\nStarting minifabric 2.1\n'
+./minifab netup -e true -i 2.1
+
+#echo -e  '\nInstalling jq...\n'
+#docker exec -it mysite apt-get update
+#docker exec -it mysite apt-get --assume-yes install jq
+
+echo -e '\nNetwork status should work now\n'
+./minifab stats
+
+echo -e '\nCreating the channel\n'
+./minifab create
+./minifab join
+./minifab anchorupdate
+./minifab profilegen
