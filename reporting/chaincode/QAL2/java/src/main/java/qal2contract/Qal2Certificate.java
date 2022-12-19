2171fbb75cdeb6954267a7b419e36da872c31da4
janzumbier <janbiermann24@gmail.com>
2022-12-19 16:42:57 +0100

    Docs

--- reporting/chaincode/QAL2/java/src/main/java/qal2contract/Qal2Certificate.java
+++ reporting/chaincode/QAL2/java/src/main/java/qal2contract/Qal2Certificate.java
@@ -42,51 +42,101 @@ public class Qal2Certificate {
         this(sensorid, ownerorg, inspectorcert, inspectororganisation, null);
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getSensorid() {
         return sensorid;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getOwnerorg() {
         return ownerorg;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getInspectororganisation() {
         return inspectororganisation;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getInspectorcert() {
         return inspectorcert;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getExpirydate() {
         return expirydate;
     }
 
+    
+    /** 
+     * @param inspectorcert
+     */
     public void setInspectorcert(String inspectorcert) {
         this.inspectorcert = inspectorcert;
     }
 
+    
+    /** 
+     * @param inspectororganisation
+     */
     public void setInspectororganisation(String inspectororganisation) {
         this.inspectororganisation = inspectororganisation;
     }
 
+    
+    /** 
+     * @param expirydate
+     */
     public void setExpirydate(String expirydate) {
         this.expirydate = expirydate;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String toJson(){
         return new Gson().toJson(this);
     }
 
+    
+    /** 
+     * @param jsonstring
+     * @return Qal2Certificate
+     */
     public static Qal2Certificate fromJSON(String jsonstring){
         return new Gson().fromJson(jsonstring, Qal2Certificate.class);
     }
 
+    
+    /** 
+     * @return String
+     */
     @Override
     public String toString() {
         return this.toJson();
     }
 
+    
+    /** 
+     * @param obj
+     * @return boolean
+     */
     @Override
     public boolean equals(Object obj) {
         if(this == obj){
@@ -103,6 +153,10 @@ public class Qal2Certificate {
                                  ,new String[] {other.sensorid, other.ownerorg, other.inspectorcert, other.inspectororganisation, other.expirydate});
     }
 
+    
+    /** 
+     * @return int
+     */
     @Override
     public int hashCode() {
         return Objects.hash(this.sensorid, this.ownerorg, this.inspectorcert, this.inspectororganisation, this.expirydate);
--- reporting/chaincode/QAL2/java/src/main/java/qal2contract/Qal2Contract.java
+++ reporting/chaincode/QAL2/java/src/main/java/qal2contract/Qal2Contract.java
@@ -20,7 +20,6 @@ import org.hyperledger.fabric.contract.annotation.Default;
 import org.hyperledger.fabric.contract.annotation.Info;
 import org.hyperledger.fabric.contract.annotation.License;
 import org.hyperledger.fabric.contract.annotation.Transaction;
-import org.hyperledger.fabric.shim.ChaincodeException;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import org.hyperledger.fabric.shim.ledger.CompositeKey;
 
@@ -44,9 +43,16 @@ public class Qal2Contract implements ContractInterface {
 
         private final String keyprefix = "org~id";
 
+        Helper helper = new Helper();
+
         private static Log _logger = LogFactory.getLog(Qal2Certificate.class);
 
 
+        
+        /** 
+         * @param context Hyperledger Fabric context
+         * @return String
+         */
         @Transaction(intent = Transaction.TYPE.SUBMIT)
         public String init(final Context context){
 
@@ -58,52 +64,85 @@ public class Qal2Contract implements ContractInterface {
         }
 
 
+        
+        /** 
+         * Creates a QAL2Certificate
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensorid
+         * @param ownerorg Owner organisation of the sensor
+         * @return String Response
+         */
         @Transaction(intent = Transaction.TYPE.SUBMIT)
-        public void createCertificate(Context ctx, String sensorid, String ownerorg){
+        public String createCertificate(Context ctx, String sensorid, String ownerorg){
                 ChaincodeStub stub = ctx.getStub();
 
                 if(ownerorg == stub.getMspId()){
-                        throw new ChaincodeException("You are not allowed to create a certificate for yourself");
+                       return helper.createFailureResponse("You are not allowed to create a certificate for yourself");
                 }
 
-                if(certificateExists(ctx, sensorid, ownerorg)){
-                        throw new ChaincodeException("Certificate already exists");
+                if(helper.isSuccess(certificateExists(ctx, sensorid, ownerorg))){
+                        helper.createFailureResponse("Certificate already exists");
                 }
 
                 Qal2Certificate certificate = new Qal2Certificate(sensorid, ownerorg);
                 CompositeKey key = new CompositeKey(keyprefix, new String[] {ownerorg, sensorid});
                 stub.putStringState(key.toString(), certificate.toJson());
+                return helper.createSuccessResponse("Certificate created successfully");
         }
 
+        
+        /** 
+         * Updates the inspector section of the Qal2Cerificate
+         * 
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensor id
+         * @param ownerorg Owner of the sensor
+         * @param inspectorcert Inspection secret
+         * @param inspectororganisation Organisation of the inspector
+         * @return String response
+         */
         @Transaction(intent = Transaction.TYPE.SUBMIT)
-        public void updateInspector(Context ctx, String sensorid, String ownerorg, String inspectorcert, String inspectororganisation) throws NoSuchAlgorithmException{
+        public String updateInspector(Context ctx, String sensorid, String ownerorg, String inspectorcert, String inspectororganisation) {
                 
                 ChaincodeStub stub = ctx.getStub();
 
                 if(ownerorg == stub.getMspId()){
-                        throw new ChaincodeException("You are not allowed to update the inspector");
+                        return helper.createFailureResponse("You are not allowed to update the inspector");
                 }
 
-                if(!certificateExists(ctx, sensorid, ownerorg)){
-                        throw new ChaincodeException("Certificate does not exist");
+                if(!helper.isSuccess(certificateExists(ctx, sensorid, ownerorg))){
+                        return helper.createFailureResponse("Certificate does not exist");
                 }
 
                 Qal2Certificate currentcertificate = Qal2Certificate.fromJSON(getCertificate(ctx, sensorid, ownerorg));
 
-                CompositeKey key = new CompositeKey(keyprefix, new String[] {ownerorg, sensorid});
-                
-                
                 String salted = inspectorcert.concat(inspectororganisation);
-                MessageDigest digest = MessageDigest.getInstance("SHA-256");
-                byte[] hashedcert = digest.digest(salted.getBytes(StandardCharsets.UTF_8));
+                MessageDigest digest;
+                try {
+                        digest = MessageDigest.getInstance("SHA-256");
+                        byte[] hashedcert = digest.digest(salted.getBytes(StandardCharsets.UTF_8));
+                        Qal2Certificate certificate = new Qal2Certificate(sensorid, ownerorg, new String(hashedcert, StandardCharsets.UTF_8), inspectororganisation, currentcertificate.getExpirydate());
+                        stub.putStringState(helper.createCompositeKey(keyprefix, sensorid, ownerorg).toString(), certificate.toJson());
+                } catch (NoSuchAlgorithmException e) {
+                        return helper.createFailureResponse(e.getMessage());
+                }
 
-                Qal2Certificate certificate = new Qal2Certificate(sensorid, ownerorg, new String(hashedcert, StandardCharsets.UTF_8), inspectororganisation, currentcertificate.getExpirydate());
-                stub.putStringState(key.toString(), certificate.toJson());
-
+                return helper.createSuccessResponse("Inspector successfully updated");
+                
         }
 
+        
+        /**
+         * Certifies the sensor
+         *  
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensor id
+         * @param ownerorg Organisation of the sensor
+         * @param inspectorcert Inspection secret
+         * return String response
+         */
         @Transaction(intent = Transaction.TYPE.SUBMIT)
-        public void certify(Context ctx, String sensorid, String ownerorg, String inspectorcert) throws NoSuchAlgorithmException{
+        public String certify(Context ctx, String sensorid, String ownerorg, String inspectorcert) throws NoSuchAlgorithmException{
 
                 ChaincodeStub stub = ctx.getStub();
                 final ClientIdentity clientIdentity = ctx.getClientIdentity();
@@ -111,16 +150,16 @@ public class Qal2Contract implements ContractInterface {
                 final String checkid = clientIdentity.getX509Certificate().getSubjectDN().getName();
                 final String isid = "CN="+sensorid+", OU=client";
 
-                if(!certificateExists(ctx, sensorid, ownerorg)){
-                        throw new ChaincodeException("The certificate does not exists");
+                if(!helper.isSuccess(certificateExists(ctx, sensorid, ownerorg))){
+                        return helper.createFailureResponse("The certificate does not exists");
                 }
 
                 if(!cmspID.equals(ownerorg)){
-                        throw new ChaincodeException("Your organisation is not allowed to certify this device");
+                        return helper.createFailureResponse("Your organisation is not allowed to certify this device");
                 }
 
                 if(!isid.equals(checkid)){
-                        throw new ChaincodeException("You are not allowed to certify this device! You are not the device!");
+                        return helper.createFailureResponse("You are not allowed to certify this device! You are not the device!");
                 }
 
                 LocalDateTime time = LocalDateTime.ofInstant(stub.getTxTimestamp(), ZoneOffset.UTC);
@@ -128,7 +167,6 @@ public class Qal2Contract implements ContractInterface {
                 Instant expiryDate = time.toInstant(ZoneOffset.UTC);
 
                 Qal2Certificate currentcertificate = Qal2Certificate.fromJSON(getCertificate(ctx, sensorid, ownerorg));
-                CompositeKey key = new CompositeKey(keyprefix, new String[] {ownerorg, sensorid});
 
                 
                 String salted = inspectorcert.concat(currentcertificate.getInspectororganisation());
@@ -137,53 +175,100 @@ public class Qal2Contract implements ContractInterface {
 
                 if(hashedcert.equals(currentcertificate.getInspectorcert())){
                         currentcertificate.setExpirydate(expiryDate.toString());
-                        stub.putStringState(key.toString(), currentcertificate.toJson());
+                        stub.putStringState(helper.createCompositeKey(keyprefix, sensorid, ownerorg).toString(), currentcertificate.toJson());
+                        return helper.createSuccessResponse("Sensor was certified successfully");
                 } else {
-                        throw new ChaincodeException("Invalid Certificate");
+                        return helper.createFailureResponse("Invalid secret");
                 }
 
         }
 
+        
+        /** 
+         * Deletes a specific Qal2Certificate
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensor id
+         * @param ownerorg Organisation of the sensor
+         */
         @Transaction(intent = Transaction.TYPE.SUBMIT)
-        public void deleteCertificate(Context ctx, String sensorid, String ownerorg){
+        public String deleteCertificate(Context ctx, String sensorid, String ownerorg){
 
-                if(!certificateExists(ctx, sensorid, ownerorg)){
-                        throw new ChaincodeException("Certificate does not exist");
+                if(!helper.isSuccess(certificateExists(ctx, sensorid, ownerorg))){
+                        return helper.createFailureResponse("Certificate does not exist");
                 }
-
-                CompositeKey key = new CompositeKey(keyprefix, new String[] {ownerorg, sensorid});
                 ChaincodeStub stub = ctx.getStub();
-                stub.delState(key.toString());
+                stub.delState(helper.createCompositeKey(keyprefix, sensorid, ownerorg).toString());
+                return helper.createSuccessResponse("Certificate deleted successfully");
 
         }
 
+        
+        /**
+         * Reads a specific Qal2Certificate 
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensor id
+         * @param ownerorg Organisation of the sensor
+         * @return String response
+         */
         @Transaction(intent = Transaction.TYPE.EVALUATE)
         public String getCertificate(Context ctx, String sensorid, String ownerorg){
 
-                if(!certificateExists(ctx, sensorid, ownerorg)){
-                        throw new ChaincodeException("Certificate does not exist");
+                if(!helper.isSuccess(certificateExists(ctx, sensorid, ownerorg))){
+                        return helper.createFailureResponse("Certificate does not exist");
                 }
 
                 CompositeKey key = new CompositeKey(keyprefix, new String[] {ownerorg, sensorid});
                 ChaincodeStub stub = ctx.getStub();
-                return stub.getStringState(key.toString());
+                return helper.createSuccessResponse(stub.getStringState(key.toString()));
         }
 
+        
+        /**
+         * Checks, if a specific Qal2Certificate exists
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensor id
+         * @param ownerorg Organisation of the sensor
+         * @return String response
+         */
         @Transaction(intent = Transaction.TYPE.EVALUATE)
-        public boolean certificateExists(Context ctx, String sensorid, String ownerorg){
+        public String certificateExists(Context ctx, String sensorid, String ownerorg){
                 ChaincodeStub stub = ctx.getStub();
                 CompositeKey key = new CompositeKey(keyprefix, new String[] {ownerorg, sensorid});
                 String cert = stub.getStringState(key.toString());
-                return (cert != null && cert.length() > 0);
+                if(cert != null && cert.length() > 0){
+                        return helper.createSuccessResponse("Certificate exist");
+                }
+                return helper.createFailureResponse("Certificate does not exist");
         }
 
+        
+        /**
+         * Checks, if a specific Qal2Certificate is valid 
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensor id
+         * @param ownerorg Organisation of the sensor
+         * @return String response
+         */
         @Transaction(intent = Transaction.TYPE.EVALUATE)
-        public boolean isValid(Context ctx, String sensorid, String ownerorg){
-                return isExpired(ctx, sensorid, ownerorg);
+        public String isValid(Context ctx, String sensorid, String ownerorg){
+                String response = isExpired(ctx, sensorid, ownerorg);
+                if(helper.isSuccess(response)){
+                        return helper.createSuccessResponse("valid");
+                } else {
+                        return helper.createFailureResponse(helper.parseJson(response).get("response").getAsString());
+                }
         }
 
+        
+        /**
+         * Checks, if a specific Qal2Certificate is expired 
+         * @param ctx Hyperledger fabric context
+         * @param sensorid Unique sensor id
+         * @param ownerorg Organisation of the sensor
+         * @return String response
+         */
         @Transaction(intent = Transaction.TYPE.EVALUATE)
-        public boolean isExpired(Context ctx, String sensorid, String ownerorg){
+        public String isExpired(Context ctx, String sensorid, String ownerorg){
 
                 ChaincodeStub stub = ctx.getStub();
                 Instant time = stub.getTxTimestamp();
@@ -193,19 +278,21 @@ public class Qal2Contract implements ContractInterface {
 
                 if(certificate.getExpirydate() == null){
                         _logger.info("Certificate time null");
-                        return false;
+                        return helper.createFailureResponse("Certificate time null");
                 }
-
-                _logger.info("Certificate time not null");
 
                 try {
                         Instant expire = Instant.parse(certificate.getExpirydate());
                         _logger.info("Certificate time loaded");
-                        return time.isBefore(expire);
+                        if(time.isBefore(expire)){
+                                return helper.createSuccessResponse("not expired");
+                        } else {
+                                return helper.createFailureResponse("expired");
+                        }
 
                 } catch (DateTimeParseException e) {
                         _logger.info("Error format incorrect");
-                        return false;
+                        return helper.createFailureResponse("Date format error");
                 }
 
 
